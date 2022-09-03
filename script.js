8948bab69c023677a69d2a82ad5a093924ba385e
Lucas-Evangelista99 <Lucas_Evangelista99@hotmail.com>
2022-09-03 04:34:24 -0300

    Implement grid changing and clearing

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
-let size = 16;
 const painting = document.getElementById("content");
+let size = 16;
 
 function createGrid (rows, columns) {
     painting.style.setProperty("--rows", rows);
     painting.style.setProperty("--columns", columns);
     for (let i = 1; i <= (rows * columns); i++) {
         let square = document.createElement("div");
+        square.addEventListener("mouseover", paint);
         painting.appendChild(square).className = "squares";
     }
 }
 
 createGrid(size, size);
+
+function paint(e) {
+    e.target.style.backgroundColor = "black";
+}
+
+function clear() {
+    painting.replaceChildren();
+    createGrid(size, size);
+};
+
+function gridSize(newSize) {
+    let answer = prompt("Enter a number between 1 and 100:");
+    if (answer < 1 || answer > 100) {
+        while (answer < 1 || answer > 100) {
+            answer = prompt("ERROR: Invalid number. Enter a valid number(between 1 and 100).");
+        }
+        clear();
+        newSize = parseInt(answer);
+        size = newSize
+        createGrid(size, size);
+    } else if (isNaN(answer) == true) {
+        while (isNaN(answer) == true) {
+            answer = prompt("ERROR: Invalid value. Enter a number between 1 and 100:");
+        }
+        clear();
+        newSize = parseInt(answer);
+        size = newSize
+        createGrid(size, size);
+    } else {
+        clear();
+        newSize = parseInt(answer);
+        size = newSize
+        createGrid(size, size);
+    }
+}
+
+let sizeButton = document.getElementById("size");
+sizeButton.addEventListener("click", gridSize);
+let clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clear);
