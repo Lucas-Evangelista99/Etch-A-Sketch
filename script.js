e06b0793582c86bf453f36dc5a465abc7647a582
Lucas-Evangelista99 <Lucas_Evangelista99@hotmail.com>
2022-09-07 02:26:42 -0300

    Add random color mode

--- script.js
+++ script.js
@@ -6,20 +6,44 @@ function createGrid (rows, columns) {
     painting.style.setProperty("--columns", columns);
     for (let i = 1; i <= (rows * columns); i++) {
         let square = document.createElement("div");
-        square.addEventListener("mouseover", paint);
         painting.appendChild(square).className = "squares";
     }
 }
 
-createGrid(size, size);
-
-function paint(e) {
+function black(e) {
     e.target.style.backgroundColor = "black";
 }
+
+function paintBlack() {
+    let squareArr = [...painting.children];
+    for (let i = 0; i < squareArr.length; i++) {
+        squareArr[i].addEventListener("mouseover", black);
+    }
+}
+
+function random(e) {
+    let first = Math.floor(Math.random() * 256);
+    let second = Math.floor(Math.random() * 256);
+    let third = Math.floor(Math.random() * 256);
+
+    e.target.style.backgroundColor = `rgb(${first},${second},${third})`;
+
+}
+
+function paintRandom() {
+    let squareArr = [...painting.children];
+    for (let i = 0; i < squareArr.length; i++) {
+        squareArr[i].addEventListener("mouseover", random);
+    }
+}
+
+createGrid(size, size);
+paintBlack();
 
 function clear() {
     painting.replaceChildren();
     createGrid(size, size);
+    paintBlack();
 };
 
 function gridSize(newSize) {
@@ -32,6 +56,7 @@ function gridSize(newSize) {
         newSize = parseInt(answer);
         size = newSize
         createGrid(size, size);
+        paintBlack();
     } else if (isNaN(answer) == true) {
         while (isNaN(answer) == true) {
             answer = prompt("ERROR: Invalid value. Enter a number between 1 and 100:");
@@ -40,11 +65,13 @@ function gridSize(newSize) {
         newSize = parseInt(answer);
         size = newSize
         createGrid(size, size);
+        paintBlack();
     } else {
         clear();
         newSize = parseInt(answer);
         size = newSize
         createGrid(size, size);
+        paintBlack();
     }
 }
 
@@ -52,3 +79,5 @@ let sizeButton = document.getElementById("size");
 sizeButton.addEventListener("click", gridSize);
 let clearButton = document.getElementById("clear");
 clearButton.addEventListener("click", clear);
+let randomButton = document.getElementById("random");
+randomButton.addEventListener("click", paintRandom);
